f27b97192ad8a82875dabdfd5ad22a9f18dea779
Christopher <35282686+christophermeza@users.noreply.github.com>
2020-07-15 14:14:31 -0600

    finalizing

--- CapstoneProject/scripts/close_window.js
+++ CapstoneProject/scripts/close_window.js
@@ -1,6 +1,12 @@
-function closeCurrentTab(){
-    var conf=confirm("Are you sure, you want to close this tab?");
-    if(conf==true){
-        close();
-    }
+//function closeCurrentTab(){
+  //  var conf=confirm("Are you sure, you want to close this tab?");
+    //if(conf==true){
+      //  close();
+    //}
+//}
+
+
+function windowClose() {
+window.open('','_parent','');
+window.close();
 }
